ab325e25f6857304f8560d2b79cdb190414bb98a
sakshiparaye <sakshiparaye1@gmail.com>
2021-10-29 07:23:03 +0530

    blog template and portfolio static commit

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     body = models.TextField(max_length=1000)
     image = models.ImageField(upload_to='images/')
 
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
